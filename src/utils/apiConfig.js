52377e596dd204996a9c1298bc4fc1650d926736
Sarvesh Dwivedi <sarveshd@smartdatainc.net>
2018-02-26 20:17:44 +0530

    Adding new changes

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 
 //constants for api URL
-let BASEURL = 'http://172.10.233.57:3000/';
+let BASEURL = 'http://localhost:3000/';
 
 export const GET_USER_PROFILE = BASEURL + 'readData';
 export const UPDATE_USER_NAME = BASEURL + 'updateData';
